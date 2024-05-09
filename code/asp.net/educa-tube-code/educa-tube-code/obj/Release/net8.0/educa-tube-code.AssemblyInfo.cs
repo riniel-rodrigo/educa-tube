bf110e819b3d6ebaec07ed0e360b380419159760
Pedro de Oliveira Rosas <81116171+8BitsSpike@users.noreply.github.com>
2024-05-09 06:25:28 -0300

    update

--- code/asp.net/educa-tube-code/educa-tube-code/obj/Release/net8.0/educa-tube-code.AssemblyInfo.cs
+++ code/asp.net/educa-tube-code/educa-tube-code/obj/Release/net8.0/educa-tube-code.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("680945b4-109f-462e-95ee-6c80e19d14ef")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("educa-tube-code")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b19ebcbb5773c0f434df4ebad2f3686b7db2096")]
 [assembly: System.Reflection.AssemblyProductAttribute("educa-tube-code")]
