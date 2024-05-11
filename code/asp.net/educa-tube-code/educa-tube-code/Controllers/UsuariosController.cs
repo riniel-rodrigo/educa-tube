093135c00abbff259ec02cea5d6d5b9724248a0f
Leticia Mateus <145443338+Let788@users.noreply.github.com>
2024-05-11 23:22:54 +0100

    Criando Controllers e Views Usuarios

---
+++ code/asp.net/educa-tube-code/educa-tube-code/Controllers/UsuariosController.cs
@@ -0,0 +1,23 @@
+﻿using educa_tube_code.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace educa_tube_code.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly AppDbContext _context;
+        public UsuariosController(AppDbContext context)
+        {
+            _context = context;
+            
+        }
+        public async Task<IActionResult> Index()
+        {
+            var dados = await _context.Usuarios.ToListAsync();
+
+            return View(dados); 
+        }
+    }
+}
