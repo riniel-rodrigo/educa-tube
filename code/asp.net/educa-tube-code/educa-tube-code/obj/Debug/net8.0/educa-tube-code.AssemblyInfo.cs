264ca2b3f59751a9a6b08c244c2aa22d3ce64c0d
Leticia Mateus <145443338+Let788@users.noreply.github.com>
2024-05-12 18:37:47 +0100

    commite git ignore

--- code/asp.net/educa-tube-code/educa-tube-code/obj/Debug/net8.0/educa-tube-code.AssemblyInfo.cs
+++ code/asp.net/educa-tube-code/educa-tube-code/obj/Debug/net8.0/educa-tube-code.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("educa-tube-code")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b22aef7c3622e8fd9970183522d2bfb06de643b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5922f3ab2c894e7531ee1168c82430455aac8c25")]
 [assembly: System.Reflection.AssemblyProductAttribute("educa-tube-code")]
 [assembly: System.Reflection.AssemblyTitleAttribute("educa-tube-code")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
