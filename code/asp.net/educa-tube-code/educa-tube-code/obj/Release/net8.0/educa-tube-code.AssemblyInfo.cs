5c9a7bb6f4d3a9352d240a63f0901f33dedaea6e
Pedro de Oliveira Rosas <81116171+8BitsSpike@users.noreply.github.com>
2024-05-11 21:49:45 -0300

    Updated class names

--- code/asp.net/educa-tube-code/educa-tube-code/obj/Release/net8.0/educa-tube-code.AssemblyInfo.cs
+++ code/asp.net/educa-tube-code/educa-tube-code/obj/Release/net8.0/educa-tube-code.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("educa-tube-code")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a952458f2542efda9543cee5d0ba4a2cbff2ebf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c56eaccb876c295f61af4d59ca4393fae6d1a18c")]
 [assembly: System.Reflection.AssemblyProductAttribute("educa-tube-code")]
 [assembly: System.Reflection.AssemblyTitleAttribute("educa-tube-code")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
