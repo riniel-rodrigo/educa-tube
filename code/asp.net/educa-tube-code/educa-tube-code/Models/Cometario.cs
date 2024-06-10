a083581a6883768b52df3245cc40a81d6e647a40
Pedro de Oliveira Rosas <81116171+8BitsSpike@users.noreply.github.com>
2024-06-09 21:55:06 -0300

    update

---
+++ code/asp.net/educa-tube-code/educa-tube-code/Models/Cometario.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace educa_tube_code.Models
+{
+    [Table("Comentarios")]
+    public class Cometario
+    {
+        [Key]
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+
+        [Display(Name = "Usuário")]
+        [ForeignKey("UsuarioId")]
+        public Usuario Usuario { get; set; }
+        public DateTime Data {  get; set; } = DateTime.Now;
+        [Display(Name = "Comentário")]
+        [Required(ErrorMessage = "Obrigatório escrever um comentário ou cancelar.")]
+        [MaxLength(1000, ErrorMessage = "São permitidos no máximo 1000 caracteres.")]
+        public string texto { get; set; }
+
+    }
+}
