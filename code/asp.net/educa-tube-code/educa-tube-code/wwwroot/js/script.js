84eb98884a0e15ebc565fc50880eee07150087e8
Pedro de Oliveira Rosas <81116171+8BitsSpike@users.noreply.github.com>
2024-06-09 23:14:33 -0300

    pomodotime

---
+++ code/asp.net/educa-tube-code/educa-tube-code/wwwroot/js/script.js
@@ -0,0 +1,9 @@
+
+funcition pomoAtivador(){
+    let ativador = document.getElementById("pomodo-tela");
+    if ((ativador.style.display = "none")) {
+        ativador.style.display = "flex";
+    } else {
+        ativador.style.display = "none";
+    }
+}
