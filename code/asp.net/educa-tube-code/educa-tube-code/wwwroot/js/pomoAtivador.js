636324fe8f9570af550b4fe6146f588f79637b99
Pedro de Oliveira Rosas <81116171+8BitsSpike@users.noreply.github.com>
2024-06-10 19:40:30 -0300

    update pomodorometro

---
+++ code/asp.net/educa-tube-code/educa-tube-code/wwwroot/js/pomoAtivador.js
@@ -0,0 +1,8 @@
+﻿function pomoAtivador() {
+    let ativador = document.getElementById("pomomo");
+    if (ativador.style.display == "none") {
+        ativador.style.display = "flex";
+    } else {
+        ativador.style.display = "none";
+    }
+}
--- code/asp.net/educa-tube-code/educa-tube-code/wwwroot/js/script.js
+++ code/asp.net/educa-tube-code/educa-tube-code/wwwroot/js/script.js
@@ -1,9 +1,1 @@
 
-funcition pomoAtivador(){
-    let ativador = document.getElementById("pomodo-tela");
-    if ((ativador.style.display = "none")) {
-        ativador.style.display = "flex";
-    } else {
-        ativador.style.display = "none";
-    }
-}
